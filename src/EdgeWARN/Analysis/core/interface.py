f8b90e6589e8ea5c616d313ce0e7618dc430f933
ewsofficial <ewsbackend@gmail.com>
2025-09-26 20:58:54 -0400

    Push Analysis and GUI test scripts

--- src/EdgeWARN/Analysis/core/interface.py
+++ src/EdgeWARN/Analysis/core/interface.py
@@ -47,20 +47,20 @@ class CellInformationRetriever:
             return None
 
     @staticmethod
-    def get_storm_data(storm_history, variable_name):
+    def get_storm_data(storm_history, key):
         """
         Retrieves specific data from storm history entries
         Inputs:
         - storm_history: list of storm history entries
-        - variable_name: name of data being retrieved 
+        - key: key of data being retrieved 
         Returns:
         - list of tuples: (value, datetime_object) for each entry where the variable exists
         """
         data = []
         try:
             for entry in storm_history:
-                if variable_name in entry:
-                    value = entry[variable_name]
+                if key in entry:
+                    value = entry[key]
                     ts = entry.get('timestamp')
                     if ts:
                         # Convert timestamp from ISO format back to datetime object
@@ -68,7 +68,34 @@ class CellInformationRetriever:
                         data.append((value, timestamp))
             return data
         except Exception as e:
-            print(f"Error retrieving {variable_name} from storm history: {e}")
+            print(f"Error retrieving {key} from storm history: {e}")
+            return []
+
+    def get_probsevere_data(storm_history, key):
+        """
+        Retrieves a probsevere data key from storm history
+        Inputs:
+         - storm_history: list of storm history entries
+         - key: data key
+        Returns:
+         - list of tuples: (value, datetime_object) for each entry, with non-values being "N/A"
+        """
+        data = []
+        try:
+            for entry in storm_history:
+                if "probsevere_details" in entry:
+                    value = entry["probsevere_details"][key]
+                    ts = entry.get('timestamp')
+                    if ts:
+                        # Convert ISOFormat -> datetime object
+                        timestamp = datetime.fromisoformat(ts)
+                        data.append((value, timestamp))
+                    else:
+                        data.append((value, "N/A"))
+            return data
+        
+        except Exception as e:
+            print(f"ERROR: Could not retrieve {key} from storm history: {e}")
             return []
 
 class CellInformationSaver:
@@ -106,3 +133,26 @@ class CellInformationSaver:
                         history_entry["analysis"] = {}
         
         return data
+    
+    @staticmethod
+    def save_data(storm_cells, data, cell, key):
+        """
+        Saves data under the 'analysis' key in latest storm_history
+
+        Args:
+         - storm_cells: list of storm cell dictionaries
+         - data: the final data you want to insert
+         - cell: cell number the data will be saved under
+         - key: key the data wil be stored under
+        """
+        for storm_cell in storm_cells:
+            if storm_cell["id"] == cell:
+                entry = storm_cell.get("storm_history")[-1]
+                if "analysis" not in entry:
+                    entry["analysis"] = {}
+                    return
+                entry["analysis"][key] = data
+                print(f"DEBUG: Saved {data} to Cell ID {cell} under key: {key}")
+                return
+    
+    
