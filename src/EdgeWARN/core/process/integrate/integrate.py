aa00380f38e9bdbf6d2759237f2252d5f7b826ee
yuchen-wei3667 <weiyu19one@gmail.com>
2025-11-25 15:33:05 -0500

    IMP: Added info prints to integrate module

--- src/EdgeWARN/core/process/integrate/integrate.py
+++ src/EdgeWARN/core/process/integrate/integrate.py
@@ -49,7 +49,7 @@ class StormCellIntegrator:
         active_cells = [
             cell for cell in storm_cells if cell.get("storm_history") and cell["storm_history"][-1]["timestamp"] == latest_ts
         ]
-        self.io_manager.write_debug(f"Integrating {output_key} data for {len(active_cells)} cells")
+        self.io_manager.write_info(f"Integrating {output_key} data for {len(active_cells)} cells")
 
         var_values = var.values
 
