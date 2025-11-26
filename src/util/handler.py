80dfe69e692fd1945ea079e50f82b1d1e8b30ad6
yuchen-wei3667 <weiyu19one@gmail.com>
2025-11-25 21:26:48 -0500

    ADD: load dataset functionality for FileHandler

--- src/util/handler.py
+++ src/util/handler.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timezone, timedelta
 from pathlib import Path
+import xarray as xr
+import cfgrib
+import netCDF4
+import json
 import re
 
 def extract_timestamp(filepath, use_timezone_utc=False, round_to_minute=False, isoformat=False):
@@ -24,3 +28,56 @@ def extract_timestamp(filepath, use_timezone_utc=False, round_to_minute=False, i
     if use_timezone_utc and dt.tzinfo is None: dt = dt.replace(tzinfo=timezone.utc)
     
     return dt.isoformat() if isoformat else dt
+
+class FileHandler:
+    def __init__(self, io_manager):
+        self.io = io_manager
+
+    def load_dataset(filepath):
+        """
+        Loads a dataset from filepath, detecting if its a GRIB, netCDF, or JSON file
+        Args:
+            filepath (str): Filepath to be loaded
+        
+        Returns:
+            xr.Dataset for GRIB and netCDF
+            JSON data for JSON
+        """
+        
+        if filepath.endswith(".json"):
+            self.io.write_info(f"Loading JSON file from {filepath}")
+            try:
+                with open(filepath, 'r') as f:
+                    ds = json.load(f)
+                    self.io.write_debug(f"Loaded JSON file from {filepath}")
+                    return ds
+
+            except Exception as e:
+                self.io.write_error(f"Failed to load JSON file: {e}")
+                return
+        
+        if filepath.endswith(".grib2"):
+            self.io.write_info(f"Loading GRIB file from {filepath}")
+            try:
+                ds = xr.open_dataset(filepath, engine="cfgrib", decode_timedelta=True)
+                self.io.write_debug(f"Loaded GRIB file from {filepath}")
+                return ds
+            
+            except Exception as e:
+                self.io.write_error(f"Failed to load GRIB file: {e}")
+                return
+        
+        if filepath.endswith(".nc"):
+            self.io.write_info(f"Loading netCDF file from {filepath}")
+            try:
+                ds = xr.open_dataset(filepath, engine="netcdf4", decode_timedelta=True)
+                self.io.write_debug(f"Loaded netCDF file from {filepath}")
+                return ds
+            
+            except Exception as e:
+                self.io.write_error(f"Faile to load netCDF file: {e}")
+                return
+
+
+        
+
