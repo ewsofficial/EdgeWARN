7b04220b128cec15613e343cf74b5a174fc30c1c
ewsofficial <ewsbackend@gmail.com>
2025-12-05 19:40:41 +0000

    ADD: Added file merging function to ingest

--- src/EdgeWARN/core/ingest/utils.py
+++ src/EdgeWARN/core/ingest/utils.py
@@ -1,5 +1,7 @@
 import re
 from datetime import datetime, timezone, timedelta
+import xarray as xr
+import netCDF4
 
 GOES_PATTERN = re.compile(r"s(\d{4})(\d{3})(\d{2})(\d{2})(\d{2})(\d{1})")
 MRMS_PATTERN = re.compile(r"(\d{4})(\d{2})(\d{2})[-_](\d{2})(\d{2})(\d{2})")
@@ -32,3 +34,26 @@ def extract_timestamp(filepath: str) -> datetime:
         return datetime(year, month, day, hour, minute, second, tzinfo=timezone.utc)
 
     return datetime.now(timezone.utc).replace(second=0, microsecond=0)
+
+def merge_files(file_list, io_manager):
+    """
+    Merge multiple xarray datasets from a list of file paths.
+
+    Args:
+        file_list (list): List of file paths to xarray-compatible
+        io_manager: IO manager for logging
+    Returns:
+        xarray.Dataset: Concatenated dataset
+    """
+    datasets = [xr.open_dataset(f, engine="netcdf4") for f in file_list]
+    if not datasets:
+        io_manager.write_error("No datasets to concatenate.")
+        return None
+    
+    merged_dataset = xr.merge(datasets)
+    io_manager.write_info(f"Merged {len(datasets)} datasets.")
+    for ds in datasets:
+        ds.close()
+    
+    return merged_dataset
+
