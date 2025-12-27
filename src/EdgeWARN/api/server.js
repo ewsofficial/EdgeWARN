0cd542612002e5738e15712a8919cf5283200a4d
yuchen-wei3667 <weiyu19one@gmail.com>
2025-12-27 18:33:48 -0500

    HOT: Change app.listen to use 0.0.0.0

--- src/EdgeWARN/api/server.js
+++ src/EdgeWARN/api/server.js
@@ -43,6 +43,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
