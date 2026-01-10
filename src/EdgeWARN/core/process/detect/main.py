bab08897790c39c2ff3aaa5e5cbe8dba2b43d566
yuchen-wei3667 <weiyu19one@gmail.com>
2026-01-10 14:51:59 -0500

    HOT: Clean stormcell directory

--- src/EdgeWARN/core/process/detect/main.py
+++ src/EdgeWARN/core/process/detect/main.py
@@ -48,6 +48,7 @@ def _detect_with_optional_probsevere(
 
 
 def main(radar_old, radar_new, ps_old, ps_new, pt_old, pt_new, lat_bounds: tuple, lon_bounds: tuple, json_output):
+    fs.clean_old_files(fs.STORMCELL_DIR, max_age_minutes=120)
     lat_min, lat_max = lat_bounds
     lon_min, lon_max = lon_bounds
 
