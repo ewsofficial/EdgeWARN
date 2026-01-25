7633c71416fe767e467b9a9f9f731e9aa684fe01
yuchen-wei3667 <weiyu19one@gmail.com>
2026-01-25 12:01:53 -0500

    BLD: Bump version to 1.3.1

--- src/run.py
+++ src/run.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
     if args.nogui:
         # No GUI mode: Print directly to console (already set up by default sys.stdout/stderr)
         try:
-            print(f"Running EdgeWARN v1.3.0")
+            print(f"Running EdgeWARN v1.3.1")
             print(f"Latitude limits: {lat_limits}, Longitude limits: {lon_limits}")
             main()
         except KeyboardInterrupt:
@@ -266,7 +266,7 @@ if __name__ == "__main__":
         ui_process.start()
         
         try:
-            print(f"Running EdgeWARN v1.3.0")
+            print(f"Running EdgeWARN v1.3.1")
             print(f"Latitude limits: {lat_limits}, Longitude limits: {lon_limits}")
             main(ui_process)
         except KeyboardInterrupt:
