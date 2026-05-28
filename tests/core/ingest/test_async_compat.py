997614862ea362b577e096d26705c0e7d3340fc8
yuchen-wei3667 <weiyu19one@gmail.com>
2026-05-28 12:11:43 -0400

    TST: Fix failing S3 test

--- tests/core/ingest/test_async_compat.py
+++ tests/core/ingest/test_async_compat.py
@@ -1,5 +1,7 @@
 import inspect
 
+import pytest
+
 from common.ingest.aws_async_compat import ensure_aiobotocore_endpoint_compat
 from aiobotocore.args import AioClientArgsCreator
 
@@ -8,6 +10,9 @@ def test_ensure_aiobotocore_endpoint_compat_adds_default_for_express_session_fla
     ensure_aiobotocore_endpoint_compat()
 
     signature = inspect.signature(AioClientArgsCreator.compute_endpoint_resolver_builtin_defaults)
-    parameter = signature.parameters["s3_disable_express_session_auth"]
+    parameter = signature.parameters.get("s3_disable_express_session_auth")
+
+    if parameter is None:
+        pytest.skip("s3_disable_express_session_auth parameter not present in this aiobotocore version")
 
     assert parameter.default is False
