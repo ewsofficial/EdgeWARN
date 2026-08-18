378c9d35a3c0d638b930d941ce4113ecf3a84fb8
yuchen-wei3667 <weiyu19one@gmail.com>
2026-08-17 23:38:12 -0400

    IMP: configure boundary wait granularity

--- src/util/runtime/timing.py
+++ src/util/runtime/timing.py
@@ -8,7 +8,7 @@ def sleep_for(total_seconds, interval=1.0):
         time.sleep(min(interval, max(0.0, end_time - time.time())))
 
 
-def sleep_until_boundary(minutes):
+def sleep_until_boundary(minutes, interval):
     now = datetime.now(timezone.utc)
     minutes_to_next = minutes - (now.minute % minutes)
     if minutes_to_next == 0 and now.second == 0 and now.microsecond == 0:
@@ -16,4 +16,4 @@ def sleep_until_boundary(minutes):
     next_run = now.replace(second=0, microsecond=0) + timedelta(minutes=minutes_to_next)
     sleep_seconds = max(0.0, (next_run - now).total_seconds())
     if sleep_seconds > 0:
-        sleep_for(sleep_seconds, interval=1.0)
+        sleep_for(sleep_seconds, interval=interval)
